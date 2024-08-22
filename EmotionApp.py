da85c2ef1259fe313510b998bf9ab1b2769f0a85
hiroki-5 <104665340+hiroki-5@users.noreply.github.com>
2024-08-22 23:22:16 +0900

    save_dataとload_dataメソッドを追加

--- EmotionApp.py
+++ EmotionApp.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-
+import json
 class EmotionApp:
     def __init__(self, root):
         self.root = root
@@ -27,6 +27,12 @@ class EmotionApp:
         self.submit_button = tk.Button(self.root, text="Submit", command=self.submit_emotion)
         self.submit_button.pack(pady=20)
 
+        self.save_button = tk.Button(self.root, text="Save Data", command=self.save_data)
+        self.save_button.pack(pady=5)
+        
+        self.load_button = tk.Button(self.root, text="Load Data", command=self.load_data)
+        self.load_button.pack(pady=5)
+        
         self.results_label = tk.Label(self.root, text="")
         self.results_label.pack(pady=20)
         
@@ -44,6 +50,20 @@ class EmotionApp:
         print(f"Total Score: {total_score}")
 
         self.results_label.config(text=f"Total Score: {total_score}")
+
+    def save_data(self):
+        with open("emotion_data.json", "w") as file:
+            json.dump(self.emotion__data, file)
+        print("Data saved successfully.")
+        
+    def load_data(self):
+        try:
+            with open("emotion_data.json", "r") as file:
+                self.emotion__data = json.load(file)
+                print("Data loaded successfully.")
+                self.results_label.config(text="data loaded successfully.")
+        except FileNotFoundError:
+                print("No data file found.")
 
 if __name__ == "__main__":
     root = tk.Tk()
